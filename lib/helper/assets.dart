eb78ce3c70d55116920c326650d984841aaa033e
mohamed essam <mohamedessam2003147@gmail.com>
2025-01-04 16:59:12 +0200

    add assets and flutter svg package

---
+++ lib/helper/assets.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsIconsBillsIcon
+  /// assets/icons/Bills_icon.svg
+  static const String assetsIconsBillsIcon = "assets/icons/Bills_icon.svg";
+
+  /// Assets for assetsIconsDeliveryScooterIcon
+  /// assets/icons/Delivery_Scooter_icon.svg
+  static const String assetsIconsDeliveryScooterIcon = "assets/icons/Delivery_Scooter_icon.svg";
+
+  /// Assets for assetsIconsFavoriteIcon
+  /// assets/icons/favorite_icon.svg
+  static const String assetsIconsFavoriteIcon = "assets/icons/favorite_icon.svg";
+
+  /// Assets for assetsIconsMessageIcon
+  /// assets/icons/message_icon.svg
+  static const String assetsIconsMessageIcon = "assets/icons/message_icon.svg";
+
+  /// Assets for assetsIconsOrderHistoryIcon
+  /// assets/icons/order_history_icon.svg
+  static const String assetsIconsOrderHistoryIcon = "assets/icons/order_history_icon.svg";
+
+  /// Assets for assetsIconsRestaurantLocationIcon
+  /// assets/icons/Restaurant_Location_icon.svg
+  static const String assetsIconsRestaurantLocationIcon = "assets/icons/Restaurant_Location_icon.svg";
+
+  /// Assets for assetsIconsSettingIcon
+  /// assets/icons/Setting_icon.svg
+  static const String assetsIconsSettingIcon = "assets/icons/Setting_icon.svg";
+}
+
